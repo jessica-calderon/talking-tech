fa458d7002899297bcf38290d91b4b28e5f45d07
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-31 03:12:56 -0500

    heroku fix attempt

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(require("./controllers/"));
 
 sequelize.sync({ force: true }).then(() => {
-  app.listen(process.env.PORT || 3000, function () {
+  app.listen(process.env.PORT || 3001, function () {
     console.log("Now listening");
   });
 });
