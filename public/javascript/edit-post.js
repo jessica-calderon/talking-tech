43d1e99c145ca53d9bd2fecdfeb3dc3c885fd7cd
jessica-calderon <calderonjessica13@yahoo.com>
2022-08-01 00:37:43 -0500

    remove upvotes and fix edit post

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,15 +1,17 @@
-/* TODO: fix edit blog post */
 async function editFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_text = document.querySelector('textarea[name="post-text"]').value;
   const id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
+
   const response = await fetch(`/api/posts/${id}`, {
     method: "PUT",
     body: JSON.stringify({
       title,
+      post_text,
     }),
     headers: {
       "Content-Type": "application/json",
